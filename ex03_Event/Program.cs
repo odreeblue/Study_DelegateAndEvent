4e405f283d9e67eb3d141086c8d61fc5e6d9da85
XINNOS_DESKTOP <cy-kim@xinnos.com>
2022-02-08 12:53:41 +0900

    study_2

--- ex03_Event/Program.cs
+++ ex03_Event/Program.cs
@@ -6,31 +6,41 @@ using System.Threading.Tasks;
 
 namespace ex03_Event
 {
-    // 한정자 event 델리게이트 이름;
-
-    public delegate void MyEventHandler(string name);
-
+    public delegate void MyEventHandler(string name,int num);// 델리게이트 원형을 표시
+    //public delegate void MyEventHandler(int num); //  delegate 오버로딩은 안됨
     class Publisher
     {
-        public event MyEventHandler Active;
+        // 한정자 event 델리게이트 이름;
+        public event MyEventHandler Active; // Active라는 event 선언
         public void DoActive(int number)
         {
-            if (number % 10 == 0)
-                Active("Active!" + number);
-            else
-                Console.WriteLine(number);
+            if (number % 10 == 0) // 10으로 나누어 떨어졌을 때
+                //Active("Active!" + number, number+1); // Active! + number 출력
+                Active("Active!" + number,number+2);
+            else//아니면
+                Console.WriteLine(number); // number만 출력
         }
     }
     internal class Program
     {
-        static public void MyHandler(string message)
+        static public void MyHandler(string message,int num) // MyHandler라는 메서드 만들고, Message를 매개변수로 받아서
         {
-            Console.WriteLine(message);
+            Console.WriteLine("받은 string:"+message+"  받은 num:"+num); // Message를 출력해라
         }
+        ////static public void MyHandler(string message)
+        ////{
+        ////    Console.WriteLine("string만 받았다 그 값은" + message);
+        ////}
+        //static public void MyHandler2(int number)
+        //{
+        //    Console.WriteLine("int 매개변수 = {0}", number);
+        //}
         static void Main(string[] args)
         {
-            Publisher publisher = new Publisher();
-            publisher.Active += new MyEventHandler(MyHandler);
+            Publisher publisher = new Publisher(); // Publisher 클래스의 객체를 publisher라는 이름으로 생성
+            
+            publisher.Active += new MyEventHandler(MyHandler); // event <- delegate <- function
+            //publisher.Active += new MyEventHandler(MyHandler2); // 
             for (int i=0; i< 50; i++)
             {
                 publisher.DoActive(i);
@@ -39,3 +49,6 @@ namespace ex03_Event
         }
     }
 }
+//      delegate <- function1
+//               <- function2
+//               <- .... function3
